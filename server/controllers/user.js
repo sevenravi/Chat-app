cbdaab8bac4607bbacf1dd497a83ea44fd69297f
ravi <ravikumarmahato7@gmail.com>
2024-01-19 18:50:32 +0545

    password hash generated

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,12 +1,22 @@
 const User = require('../models/user')
+const bcrypt = require('bcrypt');
+
+
+//controller to register new user using POST: '/register'
 const registerNewUser = async (req,res)=>{
+    //checkin existing user 
     const existingUser = await User.findOne({email:req.body.email})
     if (existingUser){
        return  res.status(403).json({
             msg:"Email already used."
         })
     }
-    await User.create(req.body)
+    const secPassword = await bcrypt.hash(myPlaintextPassword, saltRounds);
+    await User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:secPassword
+    })
     res.json({
         msg :"Successfully created new account."
     })
