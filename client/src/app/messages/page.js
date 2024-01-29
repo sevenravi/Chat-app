88ebd9bac7451284a4d8552c6b8fab9ac3b8a78e
ravi <ravikumarmahato7@gmail.com>
2024-01-29 21:51:19 +0545

    redux persist

--- client/src/app/messages/page.js
+++ client/src/app/messages/page.js
@@ -1,23 +1,39 @@
 'use client'
 import React, { useEffect, useState } from 'react'
+import { Card, CardBody } from "@nextui-org/react";
+
 const page = () => {
-  const userList=async ()=>{
-    const [list, setlist] = useState([])
+  const [list, setlist] = useState([])
+  const [selectedItem,setselectedItem]= useState(null)
+
+  const userList = async () => {
     const res = await fetch('http://localhost:5000/getuser')
-    const data =await res.json()
+    const data = await res.json()
     setlist(data)
-    console.log(list)
-    
+
   }
   useEffect(() => {
     userList()
   }, [])
-  
+
 
   return (
-    
+
     <div className='flex flex-row h-screen text-white bg-zinc-800' >
-      <div className="border-white border-1 m-8 ml-17 w-1/3" >messages</div>
+      {/* {JSON.stringify(list)} */}
+      <div className="border-white border-1 m-8 ml-17 w-1/3" >
+      {list.length>0 && list.map((item,id)=>{
+        return (<Card onPress={()=>setselectedItem(item)} className='mb-1'>
+          <CardBody>
+            
+             {item.name}
+            
+          </CardBody>
+        </Card>)
+        
+        })}
+        {console.log(selectedItem)}
+      </div>
       <div className="border-white border-1 m-8 w-2/3" >Seven D Great</div>
     </div>
   )
--- client/src/redux/reduxProvider.js
+++ client/src/redux/reduxProvider.js
@@ -1,11 +1,14 @@
 'use client'
 import { Provider } from 'react-redux'
-import store from './store'
+import { persistor, store } from './store'
+import { PersistGate } from 'redux-persist/integration/react';
 
 const ReduxProvider=({children})=>{
     return(
         <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
             {children}
+            </PersistGate>
         </Provider>
     )
 }
