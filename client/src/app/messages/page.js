a28d03ee6a70794216ba95515180cc777f057194
ravi <ravikumarmahato7@gmail.com>
2024-02-01 22:43:36 +0545

    chat UI Changes

--- client/src/app/messages/page.js
+++ client/src/app/messages/page.js
@@ -1,10 +1,10 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { Card, CardBody } from "@nextui-org/react";
+import { Card, CardBody,Divider } from "@nextui-org/react";
 
 const page = () => {
   const [list, setlist] = useState([])
-  const [selectedItem,setselectedItem]= useState(null)
+  const [selectedItem, setselectedItem] = useState(null)
 
   const userList = async () => {
     const res = await fetch('http://localhost:5000/getuser')
@@ -16,7 +16,7 @@ const page = () => {
     userList()
   }, [])
 
-  const handleOnPress =(item)=>{
+  const handleOnPress = (item) => {
     setselectedItem(item)
   }
 
@@ -24,19 +24,35 @@ const page = () => {
 
     <div className='flex flex-row h-screen text-white bg-zinc-800' >
       {/* {JSON.stringify(list)} */}
-      <div className="border-white border-1 m-8 ml-17 w-1/3" >
-      {list.length>0 && list.map((item,id)=>{
-        return (<div key={id}  onClick={()=>handleOnPress(item)}>
-          <Card  className= {selectedItem?._id == item._id ? 'mb-1 bg-blue-500':'mb-1'}>
-          <CardBody>
-             {item.name}
-            
-          </CardBody>
-        </Card></div>)
-        
-        })}
+      <div className="sidebar ml-4 m-4 mr-2">
+        <section>logout</section>
+        <section>profile</section>
       </div>
-      <div className="border-white border-1 m-8 w-2/3" >Seven D Great   {JSON.stringify(selectedItem)}</div>
+      <div className="messageBody flex flex-row  border-white border-1 rounded-2xl m-4 mr-2 w-11/12 bg-white text-black">
+
+
+        <div className="border-white border-1 m-4 mr-2 w-1/3" >
+          {list.length > 0 && list.map((item, id) => {
+            return (<div key={id} onClick={() => handleOnPress(item)}>
+              <Card className={selectedItem?._id == item._id ? 'mb-1 bg-zinc-200' : 'mb-1'}>
+                <CardBody>
+                  {item.name}
+
+                </CardBody>
+              </Card></div>)
+
+          })}
+        </div>
+        <Divider className='m-3' orientation="vertical" />
+
+        <div className="border-white border-1 m-4 mr-2 w-2/3" >Seven D Great   {JSON.stringify(selectedItem)}</div>
+
+      </div>
+      <div className="flex m-4 ml-2  w-3/12 ">
+        <div className="border-1 m-2 bg-white rounded-2xl	 text-black w-11/12  ">
+          profile view
+        </div>
+      </div>
     </div>
   )
 }
