e4370e7075fa5718836c14069789a959918fc8d8
ravi <ravikumarmahato7@gmail.com>
2024-02-24 18:07:13 +0545

    siderbar styles updated

--- client/src/app/messages/Sidebar.js
+++ client/src/app/messages/Sidebar.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {Divider} from "@nextui-org/react";
+import { IoIosLogOut } from "react-icons/io";
+import { FaUserCircle } from "react-icons/fa";
+import { FaMessage } from "react-icons/fa6";
+
 
 const Sidebar = () => {
   return (
-    <div className="sidebar ml-4 m-4 mr-2">
-        <section>logout</section>
-        <section>profile</section>
+    <div className="flex flex-col   m-2 mr-1 ">
+
+      <div className="flex-grow sidebar-body flex flex-col justify-center items-center w-16">
+      <section className='my-2 flex flex-col items-center cursor-pointer'>
+          <FaMessage color='gray' size={'30px'} />
+          <span className='text-xs text-gray-300 m-1'>All chats</span>
+        </section>
+        <Divider className="bg-gray-500" style={{ height: "2px" }}/>
+      <section className='my-2 flex flex-col items-center cursor-pointer'>
+          <FaUserCircle color='gray' size={'30px'} />
+          <span className='text-xs text-gray-300 m-1'>Profile</span>
+        </section>
       </div>
-  )
+
+      <div className="sidebar-footer">
+        <section className='my-2 flex flex-col items-center p-2 mb-10 cursor-pointer'>
+          <IoIosLogOut size={'28px'} />
+          <span className='text-xs text-gray-300 m-1'>Logout</span>
+        </section>
+      </div>
+      
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
